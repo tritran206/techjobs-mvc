c5f790d04e07141e501c5fff81aedfc01ce52d5e
Tri Tran <tritran20@gmail.com>
2017-08-25 18:46:13 -0700

    Added search "All" feature.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -22,16 +22,24 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
 
     @RequestMapping(value = "results")
     public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
-        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
-        model.addAttribute("columns", ListController.columnChoices);
-        model.addAttribute("jobs", jobs);
-        model.addAttribute("searchTerm", searchTerm);
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("jobs", jobs);
+            model.addAttribute("searchTerm", searchTerm);
+            return "search";
+        } else {
 
-        return "search";
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("jobs", jobs);
+            model.addAttribute("searchTerm", searchTerm);
+
+            return "search";
+        }
     }
 }
